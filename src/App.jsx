bb0d9837c843a3ca55a4fa58dcbb7853baa3e19b
salah <salaheddinebaha32@gmail.com>
2026-03-26 01:14:35 +0100

    feat: add @tailwindplus/elements dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return(
-     <h1 className="text-4xl text-blue-500 font-bold text-center mt-[15px]">
-      Porfolio Under Working 
-    </h1>
-  );
+  
 }
 
-export default App
+export default App;
